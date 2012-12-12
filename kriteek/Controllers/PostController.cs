fa0be6d4c8c270c5e9520652123be5a96fc973d7
edobry <edobry@gmail.com>
2012-12-11 19:02:40 -0500

    First commit
    Added MySql connector
    switched Membership/Role providers
    Set up ADO.NET Models
    Generated preliminary PostController

---
+++ kriteek/Controllers/PostController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using kriteek.Models;
+
+namespace kriteek.Controllers
+{
+    public class PostController : ApiController
+    {
+        private kriteekEntities1 db = new kriteekEntities1();
+
+        // GET api/Post
+        public IEnumerable<Post> GetPosts()
+        {
+            return db.Posts.AsEnumerable();
+        }
+
+        // GET api/Post/5
+        public Post GetPost(long id)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return post;
+        }
+
+        // PUT api/Post/5
+        public HttpResponseMessage PutPost(long id, Post post)
+        {
+            if (ModelState.IsValid && id == post.ID)
+            {
+                db.Entry(post).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Post
+        public HttpResponseMessage PostPost(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, post);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = post.ID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Post/5
+        public HttpResponseMessage DeletePost(long id)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Posts.Remove(post);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, post);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ kriteek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kriteek.WebApiApplication" Language="C#" %>
